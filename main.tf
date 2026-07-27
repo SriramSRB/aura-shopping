9be127758cd5b5362600ebdd4ea57d90ab332cb7
SriramSRB <ssrirambabu27@gmail.com>
2026-07-27 10:59:56 +0530

    Delete AWS EIP resource and output from main.tf
    Removed AWS Elastic IP resource and output from main.tf

--- main.tf
+++ main.tf
@@ -113,14 +113,3 @@ resource "aws_instance" "aura-shopping_server" {
 
     tags = { Name = "aura-shopping-server" }
 }
-
-resource "aws_eip" "aura-shopping_eip" {
-    instance = aws_instance.aura-shopping_server.id
-    domain   = "vpc"
-    tags     = { Name = "aura-shopping-eip" }
-}
-
-output "elastic_ip" {
-    value       = aws_eip.aura-shopping_eip.public_ip
-    description = "Fixed public IP - will never change on restart"
-}
